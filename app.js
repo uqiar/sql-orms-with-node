569679edc85740a07d5a9e1d1372b16bf601f256
uqair <uqair.ali@zekiexperts.com>
2020-06-01 15:42:44 -0700

    init project

---
+++ app.js
@@ -0,0 +1,32 @@
+const db = require('./db');
+const { Movie } = db.models;
+
+(async () => {
+  await db.sequelize.sync({ force: true });
+
+  try {
+    const movie = await Movie.create({
+      title: 'Toy Story',
+      runtime: 81,
+      releaseDate: '1995-11-22',
+      isAvailableOnVHS: true,
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+      title: 'The Incredibles',
+      runtime: 115,
+      releaseDate: '2004-04-14',
+      isAvailableOnVHS: true,
+    });
+    console.log(movie2.toJSON());
+
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+        const errors = error.errors.map(err => err.message);
+        console.error('Validation errors: ', errors);
+      } else {
+        throw error;
+      }
+  }
+})();
